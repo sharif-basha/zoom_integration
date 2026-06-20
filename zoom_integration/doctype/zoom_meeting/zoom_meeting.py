03a30ec02869a7c2f2a0ab28ff7801e9c2f4b227
sharif-basha <sharifbasha.achukatla@gmail.com>
2026-06-20 17:45:00 +0530

    Delete zoom_integration/doctype/zoom_meeting directory

--- zoom_integration/doctype/zoom_meeting/zoom_meeting.py
+++
@@ -1,7 +0,0 @@
-import frappe
-from frappe.model.document import Document
-
-class ZoomMeeting(Document):
-    def validate(self):
-        # Your custom backend validation logic goes here
-        pass
