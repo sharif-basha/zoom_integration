d691570e2d2d5c2c1a164a498d0efa67b768ee84
sharif-basha <sharifbasha.achukatla@gmail.com>
2026-06-20 17:29:21 +0530

    Update publisher and email in hooks.py

--- zoom_integration/hooks.py
+++ zoom_integration/hooks.py
@@ -1,8 +1,8 @@
 app_name = "zoom_integration"
 app_title = "Zoom Integration"
-app_publisher = "Your Name"
+app_publisher = "sharif"
 app_description = "Zoom Integration for Frappe"
-app_email = "your.email@example.com"
+app_email = "sharif@gmail.com"
 app_license = "MIT"
 
 # DocType Layout Home
